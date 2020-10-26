8ad34a2da5161df0aa4bea461aa153c6f65ddfd3
Rene <mr.zombie6707@gmail.com>
2020-10-26 09:43:24 -0700

    update

--- vidya/project/main.py
+++ vidya/project/main.py
@@ -33,30 +33,17 @@ def home():
     plat = game_plat
     querystring = {"platform": plat}
 
-    headers = {
-    'x-rapidapi-host': "chicken-coop.p.rapidapi.com",
-    'x-rapidapi-key': "b590a8cafamshdc9be6b93bde56bp18a072jsn109b333e1243"
-    }
+    
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     data = json.loads(response.text)
-    d_dump = json.dumps(data)
+    r_dump = json.dumps(data)
 
-    g_name = data['result'][5]
-    g_date = data['result'][1]
-    g_desc = data['result'][2]
-    g_genre = data['result'][3][0]
-    g_img = data['result'][4]
-    g_dev = data['result'][6]
-    g_rate = data['result'][8]
-
- 
-  
-
+    for r in 
     #GET https://api.rawg.io/api/platforms?key=apikey={confiig.api_key}
     #resp = requests.get(https://rawg.io/api/games?search=Warframe)
     
-    return render_template('main.html', name=current_user.name, game=g_name, gLookup=game_lookup, gPlat=game_plat)
+    return render_template('main.html', name=current_user.name, game=g_name)
 
 @main.route('/profile')
 @login_required
